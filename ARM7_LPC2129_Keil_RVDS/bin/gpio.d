0867ad25278011c4823f55374ed94a3a79c9713b
GhaadaAlaa <ghadaalaa2299@gmail.com>
2022-08-30 17:38:19 +0200

    tasks.c DONE

--- ARM7_LPC2129_Keil_RVDS/bin/gpio.d
+++
@@ -1,6 +0,0 @@
-.\bin\gpio.o: Starter_Files_V1\Starter_Files_V1\source\GPIO.c
-.\bin\gpio.o: D:\KEIL\ARM\ARMCC\Bin\..\include\stdint.h
-.\bin\gpio.o: .\Starter_Files_V1\Starter_Files_V1\lib\bit_math.h
-.\bin\gpio.o: .\Starter_Files_V1\Starter_Files_V1\header\GPIO.h
-.\bin\gpio.o: .\Starter_Files_V1\Starter_Files_V1\header\GPIO_cfg.h
-.\bin\gpio.o: D:\KEIL\ARM\INC\Philips\lpc21xx.h
